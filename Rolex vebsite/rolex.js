6f713cde8a6db2a7bd496288d3a45e0752c67593
FaridAliyev <feliyev636@gmail.com>
2024-05-27 13:38:57 +0400

    deleted

--- Rolex vebsite/rolex.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function() {
-    var navbar = document.querySelector('.navbar');
-    if (window.scrollY > 1009) {
-        navbar.style.position = 'fixed';
-        navbar.style.top = '0';
-    } else {
-        navbar.style.position = 'static';
-    }
-});
