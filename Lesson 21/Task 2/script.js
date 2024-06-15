61cd99557e5326ec59d510f8af4149f61033e026
FaridAliyev <feliyev636@gmail.com>
2024-06-15 09:32:08 +0400

    Lesson 21

---
+++ Lesson 21/Task 2/script.js
@@ -0,0 +1,22 @@
+let button = document.querySelector("button");
+
+
+
+button.style.padding = "20px"
+button.style.backgroundColor = "yellow"
+button.style.color = "blue"
+button.style.borderRadius = "10px"
+button.style.fontStyle = "italic"
+button.style.fontSize = "20px"
+
+function myFunc() {
+    let paragraf = document.getElementById("p1");
+    paragraf.style.fontSize = "30px"
+    paragraf.style.color = "orange"
+    paragraf.style.backgroundColor = "green"
+    paragraf.style.display = "block"
+    paragraf.style.textAlign = "center"
+    paragraf.style.padding = "15px"
+    paragraf.style.fontStyle = "italic"
+}
+
