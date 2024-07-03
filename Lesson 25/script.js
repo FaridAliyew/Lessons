87743d98735846d772af93723948a0e19fa9a159
FaridAliyev <feliyev636@gmail.com>
2024-07-03 10:19:14 +0400

    Lesson 25

---
+++ Lesson 25/script.js
@@ -0,0 +1,35 @@
+// let promiseMethod = fetch("https://jsonplaceholder.typicode.com/users");
+
+// promiseMethod.then((result) => {
+//     if(result == false){
+//         console.log("Sehviniz var");
+//     }
+//     return result.json();
+// }).then(data =>{
+//     console.log(data);
+// }).catch((error) => {
+//     console.log("Error"+ error);
+// });
+
+
+
+
+
+FetchApi();
+async function FetchApi(params) {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+        if (response == false) {
+            console.log("Sehviniz var");
+        }
+        let data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log("Error" + error);
+    }
+}
+
+
+
+
+
