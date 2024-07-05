b04bf6c8eedac3e29761999340d7a6024f2ce3b7
FaridAliyev <feliyev636@gmail.com>
2024-07-05 15:20:36 +0400

    Deleted

--- Mentorr/Task 1/script.js
+++
@@ -1,7 +0,0 @@
-function myFunc(params) {
-    let num1 = Number(document.getElementById("text1").value);
-    let num2 = Number(document.getElementById("text2").value);
-
-    let result = num1 + num2;
-    document.getElementById("result").innerText = result;
-}
--- Mentorr/Task 2/script.js
+++
@@ -1,5 +0,0 @@
-function myFunc(params) {
-    let img = document.createElement("img");
-    img.setAttribute("src", "Italiya.PNG");
-    document.getElementById("container").appendChild(img);
-}
