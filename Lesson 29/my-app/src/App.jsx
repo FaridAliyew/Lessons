b90c4a4c41fca339574d5be5b944a6381d2b1fe9
FaridAliyev <feliyev636@gmail.com>
2024-07-31 13:53:37 +0400

    Lesson 29

---
+++ Lesson 29/my-app/src/App.jsx
@@ -0,0 +1,12 @@
+import Register from "./Component/Register";
+import './style/style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Register/>
+    </div>
+  );
+}
+
+export default App;
